f766bc21c35ddd451d7d64e07ad3a1bac9559f1a
Vanessa Muoghara <Vanessa.Muoghara@ibm.com>
2024-09-06 13:44:57 +0100

    initial changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
+import './App.css';
+import Deck from './components/Deck/Deck';
+
+//shuffle cards
+const shuffleDeck = () => {
+  for (let i = 0; i < Deck.length; i++) {
+    let shuffle = Math.floor(Math.random() * Deck.length);
+    let temp = Deck[i];
+    Deck[i] = Deck[shuffle];
+    Deck[shuffle] = temp;
+  }
+};
+// const shuffledDeck = [...cards, ...cards]
+//   .sort(() => Math.random() - 0.5)
+//   .map((cards) => ({ ...cards, id: Math.random() }));
+
+// setCards(shuffledDeck);
+console.log('shuffleCards');
+
+const drawCard = () => {
+  console.log();
+};
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>my house of cards</h1>
+      <button className="button" onClick={shuffleDeck}>
+        SHUFFLE
+      </button>
+      <button className="button" onClick={drawCard}>
+        DRAW
+      </button>
+      <Deck />
     </div>
   );
 }
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Card.css';
+
+function Card({ suit, rank, displayText }) {
+  console.log(suit, 'suit');
+  return (
+    <div className="card">
+      <p className="rank">{rank}</p>
+      {displayText}
+      <img className="suit" src={suit} alt="suit" />
+    </div>
+  );
+}
+
+export default Card;
