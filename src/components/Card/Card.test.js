4e1c82335677e814a514f994b0cf8c903a549a8b
Vanessa Muoghara <Vanessa.Muoghara@ibm.com>
2024-10-04 09:40:24 +0100

    included describe comment to wrap the card test

--- src/components/Card/Card.test.js
+++ src/components/Card/Card.test.js
@@ -9,21 +9,40 @@ const cardData = {
   id: 'hearts_A',
 };
 
-test.only('renders card component with correct rank, suit and image link', () => {
-  render(
-    <Card
-      rank={cardData.rank}
-      suit={cardData.suit}
-      imageLink={cardData.imageLink}
-    />,
-  );
+describe('Card Component', () => {
+  beforeEach(() => {
+    // eslint-disable-next-line testing-library/no-render-in-setup
+    render(
+      <Card
+        rank={cardData.rank}
+        suit={cardData.suit}
+        imageLink={cardData.imageLink}
+      />,
+    );
+  });
 
-  // const cardRank = screen.getByText(cardData.rank);
-  // expect(cardRank).toBeInTheDocument();
+  test('displays the correct alt text for the card image', () => {
+    const cardImage = screen.getByRole('img', {
+      name: `${cardData.rank} of ${cardData.suit}`,
+    });
+    expect(cardImage).toBeInTheDocument();
+    expect(cardImage).toHaveAttribute(
+      'alt',
+      `${cardData.rank} of ${cardData.suit}`,
+    );
+  });
 
-  const cardAltText = screen.getByDisplayValue(
-    `${cardData.rank} of ${cardData.suit}`,
-  );
+  test('displays the correct image for the card', () => {
+    const cardImg = screen.getByRole('img'); // Get the image element
+    expect(cardImg).toHaveAttribute('src', cardData.imageLink); // Check the image source
+    expect(cardImg).toHaveAttribute(
+      'alt',
+      `${cardData.rank} of ${cardData.suit}`,
+    ); // Check alt text
+  });
 
-  expect(cardAltText).toBe('A of hearts');
+  test('displays the correct rank text', () => {
+    const cardRank = screen.getByText(cardData.rank);
+    expect(cardRank).toBeInTheDocument();
+  });
 });
